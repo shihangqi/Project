cbde39303f4dbd57c06f8e0fc19ad5e7a5e011e8
yangyawang <598189036@qq.com>
2016-12-03 16:15:06 +0800

    修改SceneActivity文件

--- app/src/main/java/com/example/lenovo/inequalitysign/ui/SceneActivity.java
+++ app/src/main/java/com/example/lenovo/inequalitysign/ui/SceneActivity.java
@@ -1,15 +1,40 @@
 package com.example.lenovo.inequalitysign.ui;
 
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-import com.example.lenovo.inequalitysign.R;
+import com.example.lenovo.budh.sceneContent;
+import com.example.lenovo.inequalitysign.square;
 
 public class SceneActivity extends AppCompatActivity {
+
+    private sceneContent mM;
+    private ImageView Bt_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.square_scene);
+        setContentView(R.layout.frame_scene);
+        setDefaultPage();
+        Bt_back = (ImageView)findViewById(R.id.S_back);
+        Bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SceneActivity.this,square.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setDefaultPage(){
+        android.app.FragmentManager fm=getFragmentManager();
+        FragmentTransaction transaction=fm.beginTransaction();
+        mM =new sceneContent();
+        transaction.replace(R.id.contaner2,mM);
+        transaction.commit();
     }
 }
