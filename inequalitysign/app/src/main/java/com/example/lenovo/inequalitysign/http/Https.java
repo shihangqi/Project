93d8fd91648185adb49599f0f8f815d352cb698a
shihangqi <you@example.com>
2016-12-08 10:54:48 +0800

    https类

--- inequalitysign/app/src/main/java/com/example/lenovo/inequalitysign/http/Https.java
+++ inequalitysign/app/src/main/java/com/example/lenovo/inequalitysign/http/Https.java
@@ -1,12 +1,23 @@
 package com.example.lenovo.inequalitysign.http;
 
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.lenovo.inequalitysign.R;
 import com.example.lenovo.inequalitysign.entity.Dining;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
@@ -26,26 +37,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Https {
-    private static String u = "http://10.7.88.35:8090/shop/home";
-    private String str = "3";
-    private String string="";
+
     public List<Dining> ls = new ArrayList<>();
-
-    public List<Dining> setAndGet(String u) {
-
+    private String string = "";
+    public List<Dining> setAndGet(String u,NameValuePair...pairs) {
 
         try {
             URI url = new URI(u);
             HttpClient httpclient = new DefaultHttpClient();
             HttpPost httppost = new HttpPost(url);
-            NameValuePair pair = new BasicNameValuePair("city", str);
-            List<NameValuePair> pairs = new ArrayList<>();
-            pairs.add(pair);
-            HttpEntity entity = new UrlEncodedFormEntity(pairs, "utf-8");
+            List<NameValuePair> pairss = new ArrayList<>();
+            for(NameValuePair pair:pairs){
+                pairss.add(new BasicNameValuePair(pair.getName()+"",pair.getValue()+""));
+            }
+            HttpEntity entity = new UrlEncodedFormEntity(pairss, "utf-8");
             httppost.setEntity(entity);
-            Log.e("----------1","1");
             HttpResponse response = httpclient.execute(httppost);
-            Log.e("----------2","2");
             HttpEntity httpEntity = response.getEntity();
             if (httpEntity != null) {
                 BufferedReader buffer = new BufferedReader(new InputStreamReader(httpEntity.getContent()));
@@ -55,9 +62,9 @@ public class Https {
                     string += string1;
                 }
             }
-Log.e("-----------3",string);
+
             JSONArray array = new JSONArray(string);
-            Log.e("---------------------陈晨",array.toString());
+
             for(int i =0 ; i < array.length();i++){
                 JSONObject object = array.getJSONObject(i);
                 String urll = object.getString("shop_img_small");
